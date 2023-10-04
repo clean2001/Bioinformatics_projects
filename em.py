f44e122a254c22930cd6178e9d6ac3fdc2200521
kimsejeong <clean01@hanyang.ac.kr>
2023-10-04 13:59:16 +0900

    UDT: em

--- em.py
+++ em.py
@@ -15,24 +15,28 @@ def open_and_parse(input_file: str):
     offset_hashmap = dict()
     titles = list()
     max_len = 0
-    seq = ''
     while True:
         line = f.readline().rstrip()
 
         if not line:
             f.close()
             break
-        
-        if line[0] == '>':
-            length = len(seq)
-            max_len = max(length, max_len)
 
+        if line[0] == '>':
             title = line[1:]
             offset_hashmap[title] = f.tell()
+            line = f.readline().rstrip()
             titles.append(title)
-        else:
-            seq += line
+            
+            length = 0
+            while len(line.rstrip()) >= 60:
+                line = f.readline().rstrip()
+                length += 60
+
+            length += len(line)
+            max_len = max(length, max_len)
         
+    
     return titles, offset_hashmap, max_len
 
 
@@ -69,10 +73,6 @@ def update_hidden(hidden: list, profile: list, titles: list, offset_hashmap: dic
                     print('[Error]')
                     exit(-1)
             hidden[i][j] = probability
-        
-        total = sum(hidden[i])
-        for j in range(len(nucleotide_seq)-motif_len+1):
-            hidden[i][j] /= total
 
     return hidden
 
@@ -96,13 +96,13 @@ def update_profile(hidden: list, profile: list, titles: list, offset_hashmap: di
             nucleotide_seq += line.rstrip()
         for j in range(len(nucleotide_seq)-motif_len+1):
             for k in range(motif_len):
-                if nucleotide_seq[j+k] == 'A':
+                if nucleotide_seq[k] == 'A':
                     profile[Nucleotide.A.value][k] += hidden[i][j]
-                elif nucleotide_seq[j+k] == 'C':
+                elif nucleotide_seq[k] == 'C':
                     profile[Nucleotide.C.value][k] += hidden[i][j]
-                elif nucleotide_seq[j+k] == 'G':
+                elif nucleotide_seq[k] == 'G':
                     profile[Nucleotide.G.value][k] += hidden[i][j]
-                elif nucleotide_seq[j+k] == 'T':
+                elif nucleotide_seq[k] == 'T':
                     profile[Nucleotide.T.value][k] += hidden[i][j]
                 else:
                     print('[Error]')
@@ -176,32 +176,7 @@ def init_matrix(profile:list, hidden:list, titles: list, offset_hashmap: dict, i
     hidden = update_hidden(hidden, profile, titles, offset_hashmap, input_file, motif_len)
 
     return profile, hidden
-
-
-def find_motifs(hidden, input_file, titles, offset_hashmap, motif_length):
-    motifs_array = []
-    for i in range(len(titles)):
-        max_idx, max_prob = 0, 0
-        for j in range(len(hidden[i])):
-            if max_prob < hidden[i][j]:
-                max_idx = j
-                max_prob = hidden[i][j]
-        
-        f = open(input_file, 'r')
-        f.seek(offset_hashmap[titles[i]])
-        seq = ''
-        while True:
-            line = f.readline().rstrip()
-            if not line or line[0] == '>':
-                f.close()
-                break
-            seq += line
-        
-        motif = seq[max_idx: max_idx+motif_length]
-
-        motifs_array.append(motif)
-    return motifs_array
-            
+                
 
 
 
@@ -219,40 +194,49 @@ if __name__=='__main__':
 
     # 히든 매트릭스 차원: t(유전자 개수) * (n-k+1)
     # 프로필 매트릭스 차원: 4(염기) * k
+    print(n)
     hidden_matrix = [[0 for _ in range(n-k+1)] for _ in range(t)]
     profile_matrix = [[0 for _ in range(k)] for _ in range(4)]
 
-    # print(hidden_matrix)
 
     # 초기 프로필, 히든 만들기
     init_matrix(profile_matrix, hidden_matrix, titles, offset_hashmap, input_file, k, E)
-    motifs = find_motifs(hidden_matrix, input_file, titles, offset_hashmap, k)
-    print('initial motifs', motifs)
-    for h in hidden_matrix:
-        h[0] = 1
-        
+
+    motif = ''
+    for i in range(k):
+        nucleotide, max_prob = '', 0
+        for nu in Nucleotide:
+            if max_prob < profile_matrix[nu.value][i]:
+                max_prob = profile_matrix[nu.value][i]
+                nucleotide = nu.name
+        motif += nucleotide
+
     idx = 0
     while True:
-        profile_matrix = update_profile(hidden_matrix, profile_matrix, titles, offset_hashmap, input_file, k, E)
+        update_profile(hidden_matrix, profile_matrix, titles, offset_hashmap, input_file, k, E)
 
-        new_motifs = find_motifs(hidden_matrix, input_file, titles, offset_hashmap, k)
-        flag = True
-        for i in range(len(new_motifs)):
-            if new_motifs[i] != motifs[i]:
-                flag = False
-                break
-        motifs = new_motifs
-
-        if flag:
+        new_motif = ''
+        for i in range(k):
+            nucleotide, max_prob = '', 0
+            for nu in Nucleotide:
+                if max_prob < profile_matrix[nu.value][i]:
+                    max_prob = profile_matrix[nu.value][i]
+                    nucleotide = nu.name
+            new_motif += nucleotide
+        
+        if new_motif == motif:
+            motif = new_motif
             break
-        hidden_matirx = update_hidden(hidden_matrix, profile_matrix, titles, offset_hashmap, input_file, k)
+        
+        motifs = new_motif
+        update_hidden(hidden_matrix, profile_matrix, titles, offset_hashmap, input_file, k)
         idx += 1
 
 
-    print(titles)
+    print(profile_matrix)
+    print(idx)
     print('[INFO]: EM algorithm is done.')
-    print('[INFO]: Consensus motif is', motifs)
-    print('[INFO]: iteration: ', idx)
+    print('[INFO]: motif is', motifs)
 
-    # 각 유전자에서의 모티프 찾기
-    # 끝나는 것도 내가 생각 -> ex. fractional하게 0.5개 뽑힘 -> 끝
+
+    
